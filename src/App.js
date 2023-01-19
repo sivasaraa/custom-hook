ed857e93c7fb487781941e84cc7d8337b08a2fd5
saravanan <s.k.saravanan94@gmail.com>
2023-01-19 22:35:14 +0530

    added counter js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import DocTitleOne from './Component/DocTitleOne';
 import DocTitleTwo from './Component/DocTitleTwo';
+import CounterOne from './Component/CounterOne';
 
 function App() {
   return (
     <div className="App">
-      <DocTitleOne />
-      <DocTitleTwo />
+      {/* <DocTitleOne />
+      <DocTitleTwo /> */}
+      <CounterOne />
     </div>
   );
 }
