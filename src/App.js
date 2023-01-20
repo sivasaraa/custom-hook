095a1f351408b48d37c053b5347ef8d379578c41
saravanan <s.k.saravanan94@gmail.com>
2023-01-20 21:52:03 +0530

    added user form

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import DocTitleOne from './Component/DocTitleOne';
 import DocTitleTwo from './Component/DocTitleTwo';
 import CounterOne from './Component/CounterOne';
 import CounterTwo from './Component/CounterTwo';
+import UserForm from './Component/UserForm';
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne />
       <DocTitleTwo /> */}
-      <CounterOne />
-      <CounterTwo />
+      {/* <CounterOne />
+      <CounterTwo /> */}
+      <UserForm />
     </div>
   );
 }
